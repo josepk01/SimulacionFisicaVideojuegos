58c6612488e9e26a1348fa46cfb1fda5881552d1
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-09-22 09:51:45 +0200

    Update main.cpp

--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -1,128 +1,118 @@
-#include <ctype.h>
+#include <ctype.h> // Incluye la biblioteca ctype.h
 
-#include <PxPhysicsAPI.h>
+#include <PxPhysicsAPI.h> // Incluye la biblioteca de la API de Physics (PhysX)
 
-#include <vector>
+#include <vector> // Incluye la biblioteca vector
 
-#include "core.hpp"
-#include "RenderUtils.hpp"
-#include "callbacks.hpp"
+#include "core.hpp" // Incluye el archivo core.hpp
+#include "RenderUtils.hpp" // Incluye el archivo RenderUtils.hpp
+#include "callbacks.hpp" // Incluye el archivo callbacks.hpp
 
-#include <iostream>
+#include <iostream> // Incluye la biblioteca de entrada/salida estándar
 
-std::string display_text = "This is a test";
+std::string display_text = "This is a test"; // Declara una cadena de texto llamada "display_text" con un valor inicial
 
+using namespace physx; // Usa el espacio de nombres physx
 
-using namespace physx;
+PxDefaultAllocator    gAllocator; // Declara un objeto de tipo PxDefaultAllocator llamado gAllocator
+PxDefaultErrorCallback  gErrorCallback; // Declara un objeto de tipo PxDefaultErrorCallback llamado gErrorCallback
 
-PxDefaultAllocator		gAllocator;
-PxDefaultErrorCallback	gErrorCallback;
+PxFoundation* gFoundation = NULL; // Declara un puntero a PxFoundation llamado gFoundation e inicialízalo como NULL
+PxPhysics* gPhysics = NULL; // Declara un puntero a PxPhysics llamado gPhysics e inicialízalo como NULL
 
-PxFoundation*			gFoundation = NULL;
-PxPhysics*				gPhysics	= NULL;
+PxMaterial* gMaterial = NULL; // Declara un puntero a PxMaterial llamado gMaterial e inicialízalo como NULL
 
+PxPvd* gPvd = NULL; // Declara un puntero a PxPvd llamado gPvd e inicialízalo como NULL
 
-PxMaterial*				gMaterial	= NULL;
+PxDefaultCpuDispatcher* gDispatcher = NULL; // Declara un puntero a PxDefaultCpuDispatcher llamado gDispatcher e inicialízalo como NULL
+PxScene* gScene = NULL; // Declara un puntero a PxScene llamado gScene e inicialízalo como NULL
+ContactReportCallback gContactReportCallback; // Declara un objeto de tipo ContactReportCallback llamado gContactReportCallback
 
-PxPvd*                  gPvd        = NULL;
-
-PxDefaultCpuDispatcher*	gDispatcher = NULL;
-PxScene*				gScene      = NULL;
-ContactReportCallback gContactReportCallback;
-
-
-// Initialize physics engine
+// Inicializa el motor de física
 void initPhysics(bool interactive)
 {
-	PX_UNUSED(interactive);
+    PX_UNUSED(interactive); // No se utiliza la variable 'interactive'
 
-	gFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gErrorCallback);
+    gFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gErrorCallback); // Crea una fundación de PhysX
 
-	gPvd = PxCreatePvd(*gFoundation);
-	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
-	gPvd->connect(*transport,PxPvdInstrumentationFlag::eALL);
+    gPvd = PxCreatePvd(*gFoundation); // Crea una instancia de PxPvd (Visualización de Depuración de PhysX)
+    PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10); // Crea un transporte para la visualización de depuración
+    gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL); // Conecta la instancia de PxPvd al transporte
 
-	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(),true,gPvd);
+    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd); // Crea una instancia de PxPhysics
 
-	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+    gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f); // Crea un material de física
 
-	// For Solid Rigids +++++++++++++++++++++++++++++++++++++
-	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
-	sceneDesc.gravity = PxVec3(0.0f, -9.8f, 0.0f);
-	gDispatcher = PxDefaultCpuDispatcherCreate(2);
-	sceneDesc.cpuDispatcher = gDispatcher;
-	sceneDesc.filterShader = contactReportFilterShader;
-	sceneDesc.simulationEventCallback = &gContactReportCallback;
-	gScene = gPhysics->createScene(sceneDesc);
-	}
+    // Configuración de la escena de física
+    PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = PxVec3(0.0f, -9.8f, 0.0f); // Establece la gravedad
+    gDispatcher = PxDefaultCpuDispatcherCreate(2); // Crea un despachador para la CPU
+    sceneDesc.cpuDispatcher = gDispatcher;
+    sceneDesc.filterShader = contactReportFilterShader; // Establece una función para el filtrado de colisiones
+    sceneDesc.simulationEventCallback = &gContactReportCallback; // Establece un callback para eventos de simulación
+    gScene = gPhysics->createScene(sceneDesc); // Crea una escena de física
+}
 
-
-// Function to configure what happens in each step of physics
-// interactive: true if the game is rendering, false if it offline
-// t: time passed since last call in milliseconds
+// Función para realizar un paso de física
 void stepPhysics(bool interactive, double t)
 {
-	PX_UNUSED(interactive);
+    PX_UNUSED(interactive); // No se utiliza la variable 'interactive'
 
-	gScene->simulate(t);
-	gScene->fetchResults(true);
+    gScene->simulate(t); // Simula la escena durante un tiempo 't'
+    gScene->fetchResults(true); // Recupera los resultados de la simulación
 }
 
-// Function to clean data
-// Add custom code to the begining of the function
+// Función para limpiar los datos
 void cleanupPhysics(bool interactive)
 {
-	PX_UNUSED(interactive);
+    PX_UNUSED(interactive); // No se utiliza la variable 'interactive'
 
-	// Rigid Body ++++++++++++++++++++++++++++++++++++++++++
-	gScene->release();
-	gDispatcher->release();
-	// -----------------------------------------------------
-	gPhysics->release();	
-	PxPvdTransport* transport = gPvd->getTransport();
-	gPvd->release();
-	transport->release();
-	
-	gFoundation->release();
-	}
+    gScene->release(); // Libera la escena
+    gDispatcher->release(); // Libera el despachador
+    gPhysics->release(); // Libera la instancia de PxPhysics
+    PxPvdTransport* transport = gPvd->getTransport(); // Obtiene el transporte de PxPvd
+    gPvd->release(); // Libera la instancia de PxPvd
+    transport->release(); // Libera el transporte
+    gFoundation->release(); // Libera la fundación
+}
 
-// Function called when a key is pressed
+// Función llamada cuando se presiona una tecla
 void keyPress(unsigned char key, const PxTransform& camera)
 {
-	PX_UNUSED(camera);
+    PX_UNUSED(camera); // No se utiliza la variable 'camera'
 
-	switch(toupper(key))
-	{
-	//case 'B': break;
-	//case ' ':	break;
-	case ' ':
-	{
-		break;
-	}
-	default:
-		break;
-	}
+    switch (toupper(key)) // Convierte el carácter a mayúscula y evalúa
+    {
+        //case 'B': break;
+        //case ' ':	break;
+    case ' ':
+    {
+        break;
+    }
+    default:
+        break;
+    }
 }
-
+//Funcion de manejo de colisiones (ahora vacio)
 void onCollision(physx::PxActor* actor1, physx::PxActor* actor2)
 {
-	PX_UNUSED(actor1);
-	PX_UNUSED(actor2);
+    PX_UNUSED(actor1);
+    PX_UNUSED(actor2);
 }
 
 
-int main(int, const char*const*)
+int main(int, const char* const*)
 {
-#ifndef OFFLINE_EXECUTION 
-	extern void renderLoop();
-	renderLoop();
+#ifndef OFFLINE_EXECUTION
+    extern void renderLoop(); // Declaración de una función externa llamada renderLoop
+    renderLoop(); // Llama a la función renderLoop
 #else
-	static const PxU32 frameCount = 100;
-	initPhysics(false);
-	for(PxU32 i=0; i<frameCount; i++)
-		stepPhysics(false);
-	cleanupPhysics(false);
+    static const PxU32 frameCount = 100; // Número de fotogramas para la ejecución fuera de línea
+    initPhysics(false); // Inicializa la física en modo fuera de línea
+    for (PxU32 i = 0; i < frameCount; i++)
+        stepPhysics(false); // Realiza pasos de física en modo fuera de línea
+    cleanupPhysics(false); // Limpia los datos en modo fuera de línea
 #endif
 
-	return 0;
+    return 0; // Devuelve 0 como código de salida
 }
